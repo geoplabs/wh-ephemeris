fddd0ceaf8173e83255f5f4aee39a3620d99854d
geoplabs <whathoroscope@gmail.com>
2025-08-31 21:33:59 +0530

    Revert "Add dashas and transits computation with tests"

--- api/schemas/__init__.py
+++ api/schemas/__init__.py
@@ -1,3 +1,1 @@
 from .charts import ChartInput, Place, ComputeRequest, ComputeResponse, BodyOut, MetaOut
-from .dashas import DashaComputeRequest, DashaComputeResponse
-from .transits import TransitsComputeRequest, TransitsComputeResponse
--- api/services/houses.py
+++
@@ -1,5 +0,0 @@
-def houses(jd: float, lat: float, lon: float, system: str):
-    base = jd % 360.0
-    asc = (base + lat) % 360.0
-    mc = (base + lon) % 360.0
-    return {"asc": asc, "mc": mc}
